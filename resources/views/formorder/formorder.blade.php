dea474e202987687171d49858ec916f2c0a31ef3
Petar Hamamdzhiev <petar@condor-gaming.com>
2022-10-07 00:53:49 +0300

    last last fix

--- resources/views/formorder/formorder.blade.php
+++ resources/views/formorder/formorder.blade.php
@@ -110,11 +110,11 @@
             <hr>
             {{--        2--}}
             <div>
-                <label for="number_br1">*Брой комплекти</label>
-                <input type="number" min="1" id="number_br1" name="number_br1" required>
+                <label for="number_br1">Брой комплекти</label>
+                <input type="number" min="1" id="number_br1" name="number_br1">
             </div>
             <div>
-                <label for="animal1">*Вид животни</label>
+                <label for="animal1">Вид животни</label>
                 <select id="animal1" name="animal1">
                     <option selected>Избери</option>
                     <option value="ЕПЖ">ЕПЖ</option>
@@ -124,24 +124,24 @@
                 </select>
             </div>
             <div>
-                <label for="no1">*№ на животновъдния обект:</label>
-                <input type="number" min="1" id="no1" name="no1" required>
+                <label for="no1">№ на животновъдния обект:</label>
+                <input type="number" min="1" id="no1" name="no1">
             </div>
             <div>
-                <label for="names1">*Трите имена на собственика:</label>
-                <input type="text" id="names1" name="names1" required>
+                <label for="names1">Трите имена на собственика:</label>
+                <input type="text" id="names1" name="names1">
             </div>
             <div>
-                <label for="egn1">*ЕГН на собственика:</label>
-                <input type="text" id="egn1" name="egn1" required>
+                <label for="egn1">ЕГН на собственика:</label>
+                <input type="text" id="egn1" name="egn1">
             </div>
             <div>
-                <label for="mobile1">*Телефон за връзка:</label>
-                <input type="text" id="mobile1" name="mobile1" required>
+                <label for="mobile1">Телефон за връзка:</label>
+                <input type="text" id="mobile1" name="mobile1">
             </div>
             <div>
-                <label for="ekont1">*Офис на Еконт или адрес за доставка</label>
-                <textarea name="ekont1" id="ekont1" cols="30" rows="3" required></textarea>
+                <label for="ekont1">Офис на Еконт или адрес за доставка</label>
+                <textarea name="ekont1" id="ekont1" cols="30" rows="3"></textarea>
             </div>
             <div>
                 <label for="invoice1">Данни за фактура</label>
@@ -150,11 +150,11 @@
             {{--        3--}}
             <hr>
             <div>
-                <label for="number_br2">*Брой комплекти</label>
-                <input type="number" min="1" id="number_br2" name="number_br2" required>
+                <label for="number_br2">Брой комплекти</label>
+                <input type="number" min="1" id="number_br2" name="number_br2">
             </div>
             <div>
-                <label for="animal2">*Вид животни</label>
+                <label for="animal2">Вид животни</label>
                 <select id="animal2" name="animal2">
                     <option selected>Избери</option>
                     <option value="ЕПЖ">ЕПЖ</option>
@@ -164,24 +164,24 @@
                 </select>
             </div>
             <div>
-                <label for="no2">*№ на животновъдния обект:</label>
-                <input type="number" min="1" id="no2" name="no2" required>
+                <label for="no2">№ на животновъдния обект:</label>
+                <input type="number" min="1" id="no2" name="no2">
             </div>
             <div>
-                <label for="names2">*Трите имена на собственика:</label>
-                <input type="text" id="names2" name="names2" required>
+                <label for="names2">Трите имена на собственика:</label>
+                <input type="text" id="names2" name="names2">
             </div>
             <div>
-                <label for="egn2">*ЕГН на собственика:</label>
-                <input type="text" id="egn2" name="egn2" required>
+                <label for="egn2">ЕГН на собственика:</label>
+                <input type="text" id="egn2" name="egn2">
             </div>
             <div>
-                <label for="mobile2">*Телефон за връзка:</label>
-                <input type="text" id="mobile2" name="mobile2" required>
+                <label for="mobile2">Телефон за връзка:</label>
+                <input type="text" id="mobile2" name="mobile2">
             </div>
             <div>
-                <label for="ekont2">*Офис на Еконт или адрес за доставка</label>
-                <textarea name="ekont2" id="ekont2" cols="30" rows="3" required></textarea>
+                <label for="ekont2">Офис на Еконт или адрес за доставка</label>
+                <textarea name="ekont2" id="ekont2" cols="30" rows="3"></textarea>
             </div>
             <div>
                 <label for="invoice2">Данни за фактура</label>
