d0deb7f4838e350ee12da02e0853cf204b117a91
Petar Hamamdzhiev <petar@condor-gaming.com>
2023-06-01 09:49:55 +0300

    page visits statics preparation

---
+++ app/Http/Middleware/PageVisitMiddleware.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class PageVisitMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        try {
+            DB::table('page_visits')->insert([
+                'ip' => $request->ip(),
+                'full_url' => $request->fullUrl(),
+                'user_agent' => $request->userAgent(),
+                'created_at' => now(),
+                'updated_at' => now()
+            ]);
+        } catch (\Exception $e) {}
+
+        return $next($request);
+    }
+}
---
+++ database/migrations/2023_06_01_062326_page_visits_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('page_visits', function (Blueprint $table) {
+            $table->id();
+            $table->string('ip')->index();
+            $table->string('full_url');
+            $table->string('user_agent');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('page_visits');
+    }
+};
