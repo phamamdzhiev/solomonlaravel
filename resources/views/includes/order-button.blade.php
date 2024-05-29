9b0aea8375690506a1a656dcf84de75679d3da86
Petar Hamamdzhiev <petar@condor-gaming.com>
2024-05-29 14:49:05 +0300

    fix

--- resources/views/includes/order-button.blade.php
+++ resources/views/includes/order-button.blade.php
@@ -1,7 +1,6 @@
-<a href="{{$product->features === 1 ? route('app_formorder') : route('app_product', $product->id)}}"
-   class="inline-block uppercase rounded bg-main-green-dark font-bold text-[#fff] px-6 py-1 my-4 {{$product->can_order == 0 ? 'pointer-events-none opacity-70' : ''}}"
->
+<a href="{{ $product->features == 1 ? route('app_formorder') : route('app_product', $product->id) }}"
+    class="inline-block uppercase rounded bg-main-green-dark font-bold text-[#fff] px-6 py-1 my-4 {{ $product->can_order == 0 ? 'pointer-events-none opacity-70' : '' }}">
     ПОРЪЧАЙ
 </a>
 
-{{--https://old.solomonsofia.com/bg/formlazer--}}
+{{-- https://old.solomonsofia.com/bg/formlazer --}}
--- resources/views/product/product.blade.php
+++ resources/views/product/product.blade.php
@@ -6,112 +6,107 @@
     <div class="container mt-10 max-w-[1200px]">
         @if (session('status'))
             <div class="text-center bg-main-green-dark p-3 mb-6 rounded">
-                <h1 class="text-[#fff] font-semibold text-2xl">{{session('status')}}</h1>
+                <h1 class="text-[#fff] font-semibold text-2xl">{{ session('status') }}</h1>
             </div>
         @endif
-        {{--  <div class="flex items-center my-10">--}}
+        {{--  <div class="flex items-center my-10"> --}}
         <div>
-            <img width="300px" class="rounded inline-block" src="{{asset('storage/products/' . $product->image_path )}}"
-                 alt="{{$product->name}}">
+            <img width="300px" class="rounded inline-block" src="{{ asset('storage/products/' . $product->image_path) }}"
+                alt="{{ $product->name }}">
         </div>
         <div class="mt-10">
-            <h1 class="text-2xl font-bold text-main-green-dark ">{{$product->name}}</h1>
-            @if($product->desc !== '---')
-                <h2 class="text-md my-3">{{$product->desc}}</h2>
+            <h1 class="text-2xl font-bold text-main-green-dark ">{{ $product->name }}</h1>
+            @if ($product->desc !== '---')
+                <h2 class="text-md my-3">{{ $product->desc }}</h2>
             @endif
             <h2 class="text-2xl font-semibold">
-                @if($product->price === 'Цена при запитване')
-                    {{$product->price}}
+                @if ($product->price === 'Цена при запитване')
+                    {{ $product->price }}
                 @else
-                    Цена: {{$product->price}}
+                    Цена: {{ $product->price }}
                 @endif
             </h2>
             @php
                 $isOrderButtonActive = $product->can_order == 0;
             @endphp
-            @if($product->features === 1)
-                <a href="{{$isOrderButtonActive ? '#' : route('app_formorder')}}"
-                   class="inline-block uppercase rounded bg-main-green-dark font-bold text-[#fff] px-6 py-1 my-4 {{$isOrderButtonActive ? 'pointer-events-none opacity-70' : ''}}">
+            @if ($product->features == 1)
+                <a href="{{ $isOrderButtonActive ? '#' : route('app_formorder') }}"
+                    class="inline-block uppercase rounded bg-main-green-dark font-bold text-[#fff] px-6 py-1 my-4 {{ $isOrderButtonActive ? 'pointer-events-none opacity-70' : '' }}">
                     ПОРЪЧАЙ
                 </a>
             @else
                 @if ($product->name === 'Паднали марки за ЕПЖ')
-                    <a href="{{route('app_formlazer')}}"
-                       class="inline-block uppercase rounded bg-main-green-dark font-bold text-[#fff] px-6 py-1 my-4">
+                    <a href="{{ route('app_formlazer') }}"
+                        class="inline-block uppercase rounded bg-main-green-dark font-bold text-[#fff] px-6 py-1 my-4">
                         ПОРЪЧАЙ
                     </a>
                 @else
-                    <button type="button"
-                            id="js-open-modal"
-                            class="inline-block uppercase rounded bg-main-green-dark font-bold text-[#fff] px-6 py-1 my-4 {{$isOrderButtonActive ? 'pointer-events-none opacity-70' : ''}}"
-                        @disabled($isOrderButtonActive)
-                    >
+                    <button type="button" id="js-open-modal"
+                        class="inline-block uppercase rounded bg-main-green-dark font-bold text-[#fff] px-6 py-1 my-4 {{ $isOrderButtonActive ? 'pointer-events-none opacity-70' : '' }}"
+                        @disabled($isOrderButtonActive)>
                         ПОРЪЧАЙ
                     </button>
                 @endif
             @endif
         </div>
-        {{--  </div>--}}
+        {{--  </div> --}}
     </div>
     @push('modal')
         <div id="order-modal-overlay" class="hidden">
             <div id="order-modal"
-                 class="w-full max-w-[700px] fixed bg-[#F1F2F2] z-[9000] top-1/2 left-1/2 -translate-x-1/2 p-6 py-3 border shadow-xl rounded-md -translate-y-1/2">
+                class="w-full max-w-[700px] fixed bg-[#F1F2F2] z-[9000] top-1/2 left-1/2 -translate-x-1/2 p-6 py-3 border shadow-xl rounded-md -translate-y-1/2">
                 <div class="header text-center mb-4">
                     <h1 class="text-lg font-bold uppercase text-main-green-dark font-light">Поръчка на <strong
-                            class="font-bold">{{$product->name}}</strong></h1>
+                            class="font-bold">{{ $product->name }}</strong></h1>
                 </div>
                 <div class="body">
-                    <form action="{{route('app_order_post')}}" method="post">
+                    <form action="{{ route('app_order_post') }}" method="post">
                         @csrf
-                        <input type="hidden" name="product_id" value="{{$product->id}}">
-                        <input type="hidden" name="product_name" value="{{$product->name}}">
+                        <input type="hidden" name="product_id" value="{{ $product->id }}">
+                        <input type="hidden" name="product_name" value="{{ $product->name }}">
                         <div class="flex justify-between md:flex-row flex-col">
                             <div class="pr-0 md:pr-3 w-full mb-4 md:mb-0 ">
                                 <div class="mb-4">
                                     <label class="mb-1 text-sm block font-bold" for="name">Име:</label>
                                     <input required type="text" class="w-full px-3 py-2 border rounded" id="name"
-                                           name="name"/>
+                                        name="name" />
                                 </div>
                                 <div class="mb-4">
                                     <label class="mb-1 text-sm block font-bold" for="mobile">Мобилен:</label>
                                     <input required type="text" class="w-full px-3 py-2 border rounded" id="mobile"
-                                           name="mobile"/>
+                                        name="mobile" />
                                 </div>
                                 <div>
                                     <label class="mb-1 text-sm block font-bold" for="email">Имейл:</label>
                                     <input required type="email" class="w-full px-3 py-2 border rounded" id="email"
-                                           name="email"/>
+                                        name="email" />
                                 </div>
                             </div>
                             <div class="pl-0 md:pl-3 w-full">
                                 <div class="mb-4">
                                     <label class="mb-1 text-sm block font-bold" for="quantity">Количество</label>
                                     <input required type="number" min="1" class="w-full px-3 py-2 border rounded"
-                                           id="quantity"
-                                           name="quantity"/>
+                                        id="quantity" name="quantity" />
                                 </div>
                                 <div>
                                     <label class="mb-1 text-sm block font-bold" for="message">Адрес (офис на Еконт,
                                         допълнителни
                                         данни)</label>
-                                    <textarea name="message" id="message" class="w-full px-3 py-2 border rounded"
-                                              rows="4"></textarea>
+                                    <textarea name="message" id="message" class="w-full px-3 py-2 border rounded" rows="4"></textarea>
                                 </div>
                                 <div>
-                                    <input type="checkbox" id="policy" checked/>
-                                    <label for="policy" class="font-bold">Съгласявам се с <a
-                                            href="{{route('app_policy')}}" class="text-main-green-dark uppercase"
-                                            target="_blank">общите условия</a></label>
+                                    <input type="checkbox" id="policy" checked />
+                                    <label for="policy" class="font-bold">Съгласявам се с <a href="{{ route('app_policy') }}"
+                                            class="text-main-green-dark uppercase" target="_blank">общите условия</a></label>
                                 </div>
                             </div>
                         </div>
                         <button type="submit"
-                                class="inline-block uppercase rounded bg-main-green-dark font-bold text-[#fff] px-6 py-1 my-4">
+                            class="inline-block uppercase rounded bg-main-green-dark font-bold text-[#fff] px-6 py-1 my-4">
                             ИЗПРАТИ
                         </button>
                         <button type="button" id="js-close-modal"
-                                class="inline-block uppercase rounded bg-[#bbb] font-bold px-6 py-1 my-4">
+                            class="inline-block uppercase rounded bg-[#bbb] font-bold px-6 py-1 my-4">
                             ОТКАЗ
                         </button>
                     </form>
@@ -125,12 +120,12 @@
             var body = document.querySelector('body');
 
             if (modal != null) {
-                openModalButton.addEventListener('click', function () {
+                openModalButton.addEventListener('click', function() {
                     body.classList.add('no-scrolling');
                     modal.classList.remove('hidden');
                 });
 
-                closeModalButton.addEventListener('click', function () {
+                closeModalButton.addEventListener('click', function() {
                     body.classList.remove('no-scrolling');
                     modal.classList.add('hidden');
                 });
